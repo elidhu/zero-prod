2eb2983ba910cfa2b0d1ae4b8b80d37be6b15d7b
Kevin Glasson <kevin.glasson@curtin.edu.au>
2021-03-25 22:49:43 +0800

    Checking that CI is working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero_prod::configuration::get_configuration;
+use zero_prod::startup::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Panic if we can't read the configuration.
+    let configuration = get_configuration().expect("Failed to read the configurations");
+    // The port not comes from our settings.
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    // Bubble up the std::io::Error otherwise await the result.
+    run(listener)?.await
 }
